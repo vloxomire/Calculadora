52349d0e3d5da4120b2bc6ed5404a46efc1178fe
max <vloxomire@gmail.com>
2018-08-02 19:52:26 -0300

    objetos declarados
    Declaro los objetos tipo edtText y button

--- app/src/main/java/ar/com/max/calculadora/MainActivity.java
+++ app/src/main/java/ar/com/max/calculadora/MainActivity.java
@@ -2,8 +2,12 @@ package ar.com.max.calculadora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editText1, editText2, editText3;
+    private Button button1, button2, button3, button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

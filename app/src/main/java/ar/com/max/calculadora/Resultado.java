6983e13231d705aa10250ba4668d16a1a4dd77a9
max <vloxomire@gmail.com>
2018-08-02 20:30:10 -0300

    Activity2
    Iniciado del Activity 2

---
+++ app/src/main/java/ar/com/max/calculadora/Resultado.java
@@ -0,0 +1,13 @@
+package ar.com.max.calculadora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Resultado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultado);
+    }
+}

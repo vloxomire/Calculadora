da418e68fa7d2dcfd72684046092f093a54df233
max <vloxomire@gmail.com>
2018-08-02 19:48:13 -0300

    Main Activity Xml
    Terminado el disenio del MainActivity en xml

---
+++ app/src/main/java/ar/com/max/calculadora/MainActivity.java
@@ -0,0 +1,13 @@
+package ar.com.max.calculadora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

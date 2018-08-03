ff67af44a1504b6da7fed1e228cdc4eeeb8c1bd4
max <vloxomire@gmail.com>
2018-08-02 21:56:32 -0300

    Intenterger resultado
    se crea en el resultado el Intent resultado

--- app/src/main/java/ar/com/max/calculadora/Resultado.java
+++ app/src/main/java/ar/com/max/calculadora/Resultado.java
@@ -9,5 +9,13 @@ public class Resultado extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        Bundle bolsa = new Bundle(); // creo bolsa, para recibir los clases  de suma, division, etc
+
+        bolsa = getIntent().getExtras(); // guardo los datos pasados del intent
+
+        Integer dato = bolsa.getInt("resultado");
+
+
     }
 }

3fc024b38eb76f8ba2cfe859cedd55ab4ac6adef
max <vloxomire@gmail.com>
2018-08-02 21:42:33 -0300

    Interger Division
    Se crea el Interger de la division

--- app/src/main/java/ar/com/max/calculadora/ListenerDivision.java
+++ app/src/main/java/ar/com/max/calculadora/ListenerDivision.java
@@ -13,6 +13,8 @@ public class ListenerDivision implements View.OnClickListener {
     @Override
     public void onClick(View view) {
         Intent intent = new Intent(context, Resultado.class);
-        context.startActivity(Resultado);
+        Integer division = Integer.valueOf(context.getEditText1().getText().toString()) / Integer.valueOf(context.getEditText2().getText().toString());
+        intent.putExtra("resultado",division);
+        context.startActivity(intent);
     }
 }

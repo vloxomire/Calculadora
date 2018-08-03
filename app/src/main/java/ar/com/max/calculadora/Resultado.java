6f75dbd90f206118431ff2d260c586c6f36ea52b
max <vloxomire@gmail.com>
2018-08-02 22:04:19 -0300

    Suma terminado
    se termina el setSuma

--- app/src/main/java/ar/com/max/calculadora/Resultado.java
+++ app/src/main/java/ar/com/max/calculadora/Resultado.java
@@ -2,13 +2,20 @@ package ar.com.max.calculadora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Resultado extends AppCompatActivity {
+
+    private EditText editText3;
+    private Button button5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        editText3 = findViewById(R.id.editText3);
 
         Bundle bolsa = new Bundle(); // creo bolsa, para recibir los clases  de suma, division, etc
 
@@ -16,6 +23,7 @@ public class Resultado extends AppCompatActivity {
 
         Integer dato = bolsa.getInt("resultado");
 
+            editText3.setText(String.valueOf(dato));
 
     }
 }
